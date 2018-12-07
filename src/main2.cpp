f605e357cb7ddf0893927f64b01f469de3d7f3b6
Shalagin Nikolay <qquikly@mail.ru>
2018-12-07 22:40:05 +0300

    update main2.(add result)

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	int hour = 0, min = 0;
 	int result = scanf("%d:%d", &hour, &min);
+	if (result != 2)
+	{
+		printf("Input incorrect data!");
+		return 1;
+	}
 	printf("%s", greet(hour, min));
 	return 0;
 }
